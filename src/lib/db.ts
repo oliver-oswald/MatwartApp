013039e5db873c955a69813ca26a2bb77b68b4cb
Oliver Oswald <oliver.oswald25@gmail.com>
2026-05-14 20:20:38 +0200

    fix db connection for local development

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,21 @@
+import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../../prisma/generated/prisma/client'
 
 const connectionString = `${process.env.POSTGRES_PRISMA_URL}`
+const baseConnectionString = connectionString.split('?')[0]
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient, pool: Pool }
 
-export const db = prisma
+if (!globalForPrisma.pool) {
+  globalForPrisma.pool = new Pool({
+    connectionString: baseConnectionString,
+    ssl: { rejectUnauthorized: false }
+  })
+}
+
+const adapter = new PrismaPg(globalForPrisma.pool)
+
+export const db = globalForPrisma.prisma || new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
